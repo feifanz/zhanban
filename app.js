c8abbb982897e5a9d5bf8a5aca36371ba6e2b226
feifanz <zff345579039@gmail.com>
2017-02-23 22:57:54 +1100

    1.进入聊天室里有几个可选的头像 2.显示当前是第几条 3.30秒发一次 4.管理员登入模式，底色不一样

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(session({
 }));
 
 //模板引擎
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 //静态服务
 app.use(express.static("./public"));
 
@@ -20,59 +20,83 @@ var alluser = [];
 
 //中间件
 //显示首页
-app.get("/",function(req,res,next){
-	res.render("index");
+app.get("/", function(req, res, next) {
+    res.render("index");
 });
 
-app.get("/login",function(req,res,next){
-	res.render("login");
+app.get("/post", function(req, res, next) {
+    res.render("post");
+});
+
+app.get("/login", function(req, res, next) {
+    res.render("login");
 });
 
 
 
 
 //确认登陆，检查此人是否有用户名，并且昵称不能重复
-app.get("/check",function(req,res,next){
-	var yonghuming = req.query.yonghuming;
-	var avatar = req.query.avatar;
-	if(req.session.yonghuming){
-		res.redirect("/chat");
-	}
-	if(!yonghuming){
-		res.send("必须填写用户名");
-		return;
-	}
-	if(alluser.indexOf(yonghuming) != -1){
-		res.send("用户名已经被占用");
-		return;
-	}
-	alluser.push(yonghuming);
-	//付给session
-	req.session.yonghuming = yonghuming;
-	req.session.avatar = avatar;
-	res.redirect("/chat");
+app.get("/check", function(req, res, next) {
+
+    var yonghuming = req.query.yonghuming;
+    var avatar = req.query.avatar;
+
+    console.log(req.session.denglu);
+
+    if(req.session.denglu == 1){
+        res.send("您已经登陆了~~");
+        return;
+    }
+
+    if (req.session.yonghuming == yonghuming) {
+        req.session.avatar = avatar;
+        req.session.denglu = 1;
+        res.redirect("/chat");
+        return;
+    }
+
+    if (alluser.indexOf(yonghuming) != -1) {
+        res.send("用户名已经被占用~~ 请你换个名字~~");
+        return;
+    }
+
+
+
+    alluser.push(yonghuming);
+    //付给session
+    req.session.yonghuming = yonghuming;
+    req.session.avatar = avatar;
+    req.session.denglu = 1;
+    res.redirect("/chat");
 });
 
 //聊天室
-app.get("/chat",function(req,res,next){
-	//这个页面必须保证有用户名了，
-	if(!req.session.yonghuming){
-		res.redirect("/");
-		return;
-	}
-	res.render("chat",{
-		"yonghuming" : req.session.yonghuming,
-		"avatar" : req.session.avatar
-	});
+app.get("/chat", function(req, res, next) {
+    //这个页面必须保证有用户名了，
+
+    if (!req.session.yonghuming) {
+        res.redirect("/");
+        return;
+    }
+    res.render("chat", {
+        "yonghuming": req.session.yonghuming,
+        "avatar": req.session.avatar
+    });
 })
 
-io.on("connection",function(socket){
-	socket.on("liaotian",function(msg){
-		//把接收到的msg原样广播, 1000ms延迟
-		setTimeout(function(){io.emit("liaotian",msg);}, 1000);
-		
-	});
+io.on("connection", function(socket) {
+    socket.on("liaotian", function(msg) {
+        //把接收到的msg原样广播, 1000ms延迟
+        setTimeout(function() { io.emit("liaotian", msg); }, 1000);
+
+    });
 });
+
+app.get("/logout", function(req, res, next) {
+    req.session.denglu = req.query.denglu;
+    res.send("logout");
+});
+
 
 //监听
 http.listen(3000);
